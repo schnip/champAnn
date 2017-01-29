a07ebd7c8add41adccc17b85f2adaf95b11b2ad2
mcdanitd <mcdanitd@rose-hulman.edu>
2017-01-29 17:26:20 -0500

    Debug
    Green health bars should be found.  Red and blue bars still need to be
    found.  If there is a way that we can get .png screenshots, then we
    could have the code search for the actual values of the health bar's
    color.

--- locateChampByHealthbar.m
+++ locateChampByHealthbar.m
@@ -1,4 +1,4 @@
-function [locations] = locateChampByHealthbar(img)
+%function [locations] = locateChampByHealthbar(img)
 % locateChampByHealthbar
 % Finds a champion based on the health bar. Returns the location of the
 % bounding box, based on upper left and lower right corners.
@@ -9,14 +9,14 @@ green = img(:,:,2);
 blue = img(:,:,3);
 
 locations = [0 0 0 0];
-heroNum = 1;
+heroNum = 0;
 
 % create mask based on health bar color
 mask = zeros(size(red));
-mask(blue > 250 & red < 30 & green < 30) = 1; % blue health bar
-mask(green > 250 & red < 30 & blue < 30) = 1; % green health bar
-mask(red > 250 & blue < 30 & green < 30) = 1; % red health bar
-mask(blue > 250 & red > 250 & green > 250) = 1; % black/empty health bar
+mask(blue > 200 & red < 100 & green < 100) = 1; % blue health bar
+mask(green > 150 & red < 100 & blue < 100) = 1; % green health bar
+mask(red > 200 & blue < 100 & green < 100) = 1; % red health bar
+mask(blue < 15 & red < 15 & green < 15) = 1; % black/empty health bar
 
 % Find rectangular regions
 label = bwlabel(mask);
@@ -26,30 +26,32 @@ for i=1:max(max(label))
     pixels(label == i) = 1;
     numPixels = sum(sum(pixels));
     
-    if (numPixels > 400 && numPixels < 500)
+    if (numPixels > 850 && numPixels < 950)
         % region is the right size for a health bar
+
         colNums = 1:1:size(pixels,2);
         rowNums = (1:1:size(pixels,1))';
         
         healthRows = sum(pixels,1);
         healthRows = rowNums(healthRows > 0);
-        height = max(healthRows) - min(healthRows);
+        width = max(healthRows) - min(healthRows);
         
         healthCols = sum(pixels, 2);
-        healthCols = colNums(healthRows > 0);
-        width = max(healthCols) - min(healthCols);
+        healthCols = colNums(healthCols > 0);
+        height = max(healthCols) - min(healthCols);
         
-        if (height < 20 && width < 150)
+        if (height < 15 & width < 150)
             % region has the right dimensions for a health bar
+            
+            heroNum = heroNum + 1;
             locations(heroNum,1) = min(healthCols);
             locations(heroNum,2) = min(healthRows);
             locations(heroNum,3) = max(healthCols);
             locations(heroNum,4) = max(healthRows);
-            heroNum = heroNum + 1;
         end
     end
     
 end
 
-end
+%end
 
