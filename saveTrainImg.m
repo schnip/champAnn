eda07de01feb298a19080a5a46dbe19b2c91d964
schnip <pingthegreenflag@gmail.com>
2017-01-29 17:22:51 -0500

    save Training should now be ready to go

---
+++ saveTrainImg.m
@@ -0,0 +1,21 @@
+function saveTrainImg(img, champname)
+
+foldpath = 'trainingset/';
+foldpath = strcat(foldpath, champname);
+foldpath = strcat(foldpath, '/');
+infopath = strcat(foldpath, 'info.mat');
+num = 0
+
+if exist(infopath, 'file') == 2
+	load(infopath);
+end
+
+while exit(strcat(foldpath, char(num), '.png')) == 2
+	num = num + 1;
+end
+
+imwrite(img, strcat(foldpath, char(num), '.png'));
+
+num = num + 1;
+
+save(infopath, 'num');
