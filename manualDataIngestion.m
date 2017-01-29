1e6b71d442660e0a45a6bc4175a672ecd46bc125
schnip <pingthegreenflag@gmail.com>
2017-01-29 17:03:55 -0500

    Finished manualDataIngestion.m

--- manualDataIngestion.m
+++ manualDataIngestion.m
@@ -1,5 +1,8 @@
 ingestfolder = 'ingestfolder/';
+postgestfolder = 'postgestfolder/';
 infiles = dir(ingestfolder);
+champs = textscan(fopen('champnames.txt'), '%s');
+champs = champs{1};
 
 for f = infiles'
 	f.name
@@ -7,12 +10,17 @@ for f = infiles'
 	png = strfind(f.name, 'png');
 	JPG = strfind(f.name, 'JPG');
 	jpg = strfind(f.name, 'jpg');
-	if PNG | png | JPG | jpg
+	num = size(PNG,1) + size(png,1) + size(JPG,1) + size(jpg,1);
+	if num > 0
 		img = readInImage(strcat(ingestfolder, f.name));
 		boxes = locateChampByHealthbar(img)
 		for i = 1:size(boxes,1)
 			inimg = clip(img, boxes(i,:));
 			imshow(inimg)
+			s = listdlg('PromptString', 'Which champion is this?', 'ListString', champs);
+			champname = champs{s}
+			saveTrainImg(inimg, champname);
 		end
+		movefile(strcat(ingestfolder, f.name), postgestfolder);
 	end
 end
