75e03bc6711a5575df23a589bb24e7b462aa7155
schnip <pingthegreenflag@gmail.com>
2017-02-03 02:10:03 -0500

    did some fish

--- feedClassifier.m
+++ feedClassifier.m
@@ -1,3 +1,23 @@
 infolder = 'featureset/';
 champs = textscan(fopen('champnames.txt'), '%s');
 champs = champs{1};
+data = cell(1);
+index = 1;
+
+for i = 1:size(champs,1)
+	peep = champs{i};
+	files = dir(strcat(infolder, peep));
+	for j = 1:size(files,1)
+		sample = files(j);
+		if strfind(sample.name, '.mat')
+			load(strcat(infolder, peep, '/', sample.name));
+			data(index,1) = num2cell(i);
+			temp = cell(1);
+			temp{1} = num2cell(features(:));
+			data(index,2) = temp;
+			index = index + 1;
+		end
+	end
+end
+
+% make a neural net with 136 outputs, all that jazz, you'll remember how to do this in the morning
