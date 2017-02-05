37c126ba3ab2188b990b4ea35e74690b4304868b
schnip <pingthegreenflag@gmail.com>
2017-02-05 16:40:25 -0500

    begining work on adding the NN

--- feedClassifier.m
+++ feedClassifier.m
@@ -1,7 +1,8 @@
 infolder = 'featureset/';
 champs = textscan(fopen('champnames.txt'), '%s');
 champs = champs{1};
-data = cell(1);
+data_in = [];
+data_out = [];
 index = 1;
 
 for i = 1:size(champs,1)
@@ -11,7 +12,7 @@ for i = 1:size(champs,1)
 		sample = files(j);
 		if strfind(sample.name, '.mat')
 			load(strcat(infolder, peep, '/', sample.name));
-			data(index,1) = num2cell(i);
+			data_in(index) = i;
 			temp = cell(1);
 			temp{1} = num2cell(features(:));
 			data(index,2) = temp;
@@ -21,3 +22,4 @@ for i = 1:size(champs,1)
 end
 
 % make a neural net with 136 outputs, all that jazz, you'll remember how to do this in the morning
+net = network()
