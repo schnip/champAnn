3700cc6ca5c2458448ff26ad12ec59cf0ed6796f
schnip <pingthegreenflag@gmail.com>
2017-02-05 16:46:30 -0500

    training data is now loaded in the correct format

--- feedClassifier.m
+++ feedClassifier.m
@@ -2,7 +2,7 @@ infolder = 'featureset/';
 champs = textscan(fopen('champnames.txt'), '%s');
 champs = champs{1};
 data_in = [];
-data_out = [];
+data_out = zeros(4,7*7*3);
 index = 1;
 
 for i = 1:size(champs,1)
@@ -13,13 +13,11 @@ for i = 1:size(champs,1)
 		if strfind(sample.name, '.mat')
 			load(strcat(infolder, peep, '/', sample.name));
 			data_in(index) = i;
-			temp = cell(1);
-			temp{1} = num2cell(features(:));
-			data(index,2) = temp;
+			data_out(index,:) = features(:)';
 			index = index + 1;
 		end
 	end
 end
 
 % make a neural net with 136 outputs, all that jazz, you'll remember how to do this in the morning
-net = network()
+%net = network()
