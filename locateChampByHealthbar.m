3d4643e4b65d3f74daffb675686a15f51ebb30f1
mcdanitd <mcdanitd@rose-hulman.edu>
2017-02-10 22:43:18 -0500

    9x ingestion
    creating 9 image from each champion image

--- locateChampByHealthbar.m
+++ locateChampByHealthbar.m
@@ -76,10 +76,52 @@ for i=1:max(max(label))
             right = max(healthCols) + 0;
             if (left > 50 && size(img,2) - right > 0 && size(img,1) - bottom > 200) 
                 heroNum = heroNum + 1;
-                locations(heroNum,1) = top;
-                locations(heroNum,2) = left;
-                locations(heroNum,3) = bottom;
-                locations(heroNum,4) = right;
+                
+                %centered
+                locations(heroNum*9-8,1) = top;
+                locations(heroNum*9-8,2) = left;
+                locations(heroNum*9-8,3) = bottom;
+                locations(heroNum*9-8,4) = right;
+                %up
+                locations(heroNum*9-7,1) = top-30;
+                locations(heroNum*9-7,2) = left;
+                locations(heroNum*9-7,3) = bottom-30;
+                locations(heroNum*9-7,4) = right;
+                %up right
+                locations(heroNum*9-6,1) = top-30;
+                locations(heroNum*9-6,2) = left+30;
+                locations(heroNum*9-6,3) = bottom-30;
+                locations(heroNum*9-6,4) = right+30;
+                %right
+                locations(heroNum*9-5,1) = top;
+                locations(heroNum*9-5,2) = left+30;
+                locations(heroNum*9-5,3) = bottom;
+                locations(heroNum*9-5,4) = right+30;
+                %right down
+                locations(heroNum*9-4,1) = top+30;
+                locations(heroNum*9-4,2) = left+30;
+                locations(heroNum*9-4,3) = bottom+30;
+                locations(heroNum*9-4,4) = right+30;
+                %down
+                locations(heroNum*9-3,1) = top+30;
+                locations(heroNum*9-3,2) = left;
+                locations(heroNum*9-3,3) = bottom+30;
+                locations(heroNum*9-3,4) = right;
+                %down left
+                locations(heroNum*9-2,1) = top+30;
+                locations(heroNum*9-2,2) = left-30;
+                locations(heroNum*9-2,3) = bottom+30;
+                locations(heroNum*9-2,4) = right-30;
+                %left
+                locations(heroNum*9-1,1) = top;
+                locations(heroNum*9-1,2) = left-30;
+                locations(heroNum*9-1,3) = bottom;
+                locations(heroNum*9-1,4) = right-30;
+                %left up
+                locations(heroNum*9-0,1) = top-30;
+                locations(heroNum*9-0,2) = left-30;
+                locations(heroNum*9-0,3) = bottom-30;
+                locations(heroNum*9-0,4) = right-30;
             end
         end
     end
