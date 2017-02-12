d873ad72be7ccadad122ca78ddc546948103ef6a
schnip <pingthegreenflag@gmail.com>
2017-02-12 14:44:43 -0500

    io setup

--- videoClassify.m
+++ videoClassify.m
@@ -1,1 +1,9 @@
-v = VideoReader('faker_demo.mp4');
+vr = VideoReader('faker_demo.mp4');
+vw = VideoWriter('faker_out');
+open(vw);
+while hasFrame(vr)
+	frame = readFrame(vr);
+	strcat(num2str(100 * vr.CurrentTime / vr.Duration), ' %')
+	writeVideo(vw, frame);
+end
+close(vw);
